dc75ad19332ee19ad550dd1dd742e82403bddc85
Jose Vitor <jose.vitor.sem.c@gmail.com>
2025-05-17 10:32:52 -0300

    Comparisson with normal density function

--- main.r
+++ main.r
@@ -17,9 +17,19 @@ chartSeries(ticker, theme = chartTheme("black"), type = "line")
 
 ## Plots for return value
 daily_returns <- dailyReturn(Cl(ticker))
-par(mfrow = c(2, 1))
+return_mu <- mean(daily_returns)
+return_sd <- sd(daily_returns)
+
+normal_fit_x <- seq(min(daily_returns), max(daily_returns), length.out = 100)
+normal_fit_y <- dnorm(normal_fit_x, return_mu, return_sd)
+
+par(mfrow = c(3, 1))
 plot(daily_returns, main = glue("Daily Returns of {ticker_name}"))
 hist(daily_returns, main = glue("Daily Returns of {ticker_name}"))
+plot(density(daily_returns), type = "n") # invisible plot to separate lines
+lines(density(daily_returns), col = "black", lwd = 2, )
+lines(normal_fit_x, normal_fit_y, col = "red", lwd = 2)
+par(mfrow = c(1, 1))
 
 ## Table visualization
 head(ticker)
